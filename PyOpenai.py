c8e6da3c113f137b8e45d7fd10b14ccb247fbd05
h4shell <fw1987@gmail.com>
2023-03-22 10:18:24 +0100

    Update

--- PyOpenai.py
+++ PyOpenai.py
@@ -7,12 +7,13 @@ TOKEN = config.conf['token_id']
 
 
 async def start_commmand(update, context):
-    await update.message.reply_text('Benvenuto, chiedimi quello che vuoi.')
+    await update.message.reply_text('**Benvenuto/a sono ChatGPT 3.5 Chiedimi quello che vuoi!!!**', parse_mode="Markdown")
 
 
 async def all(update, context):
     testo = update.message.text
     x = ai(testo)
+    print(x)
 
     await update.message.reply_text(x, parse_mode="Markdown")
 
@@ -23,6 +24,7 @@ if __name__ == '__main__':
 
     # Commands
     application.add_handler(CommandHandler('start', start_commmand))
+    application.add_handler(CommandHandler('ai', start_commmand))
     application.add_handler(MessageHandler(
         filters.BaseFilter(""), all))
 
