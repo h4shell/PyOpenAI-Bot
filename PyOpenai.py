4bee8e7746edad596f0dd06ae1c388eff2479f06
h4shell <fw1987@gmail.com>
2023-03-22 13:49:27 +0100

    bug fix messaggio troppo lungo telegram API

--- PyOpenai.py
+++ PyOpenai.py
@@ -13,9 +13,36 @@ async def start_commmand(update, context):
 async def all(update, context):
     testo = update.message.text
     x = ai(testo)
-    print(x)
 
-    await update.message.reply_text(x, parse_mode="Markdown")
+    parti = len(x)/4000
+    parti = int(str(parti)[0])
+    if parti <= 1:
+
+        await update.message.reply_text(x, parse_mode="Markdown")
+    elif parti > 1 and parti < 2:
+
+        testo = {0: x[:4000], 1: x[4000:]}
+        print(testo[0])
+        await update.message.reply_text(testo[0], parse_mode="Markdown")
+        await update.message.reply_text(testo[1], parse_mode="Markdown")
+        pass
+    elif parti > 2 and parti <= 3:
+
+        testo = {0: x[:4000], 1: x[4000:8000], 2: x[8000:12000]}
+        await update.message.reply_text(testo[0], parse_mode="Markdown")
+        await update.message.reply_text(testo[1], parse_mode="Markdown")
+        await update.message.reply_text(testo[2], parse_mode="Markdown")
+    else:
+        print("ERROR!! Risposta troppo lunga")
+
+        # if len(x) < 4000:
+        #     print(len(x))
+        #     await update.message.reply_text(x, parse_mode="Markdown")
+        # else:
+        #     mex1 = x[:4095]
+        #     mex2 = x[4095:]
+        #     await update.message.reply_text(mex1, parse_mode="Markdown")
+        #     await update.message.reply_text(mex2, parse_mode="Markdown")
 
 
 if __name__ == '__main__':
